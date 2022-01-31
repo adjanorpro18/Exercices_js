35ebb7a119bc3a25171c2382f12d3ef7e49841ea
adjanorpro18 <64968541+adjanorpro18@users.noreply.github.com>
2022-01-31 10:41:23 +0100

    Exercice0

---
+++ exercice_0.js
@@ -0,0 +1,3 @@
+let chaine = prompt("Merci d'inscrire une chaine de caractères dans la page !");
+//console.log(chaine)
+document.body.innerHTML = chaine;
---
+++ exercice_1.js
@@ -0,0 +1,4 @@
+
+let chaine = prompt("Merci d'inscrire une chaine de caractères dans la page !");
+document.body.innerHTML = chaine;
+
