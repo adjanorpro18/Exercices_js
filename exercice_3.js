048a3d6024bb666777d8f2bce0ca7a0e4d24d3d4
adjanorpro18 <64968541+adjanorpro18@users.noreply.github.com>
2022-01-31 11:32:33 +0100

    Exercice3

---
+++ exercice_3.js
@@ -0,0 +1,10 @@
+let budget = 1275000;
+let achats = 1250000;
+if (budget >= achats)
+{
+    alert("Le budget de"+  budget  + "€ permet de payer" +  achats + "€ d'achats !")
+}
+else
+{
+    alert("Le budget de "+   budget  +  " € ne permet pas de payer " +   achats +  " € d'achats !")
+}
