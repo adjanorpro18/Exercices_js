f613cafa775ad84664d80dad6e1efdc6e0345adc
adjanorpro18 <64968541+adjanorpro18@users.noreply.github.com>
2022-01-31 12:09:45 +0100

    Exercice6

--- exercice_4_5_6.js
+++ exercice_4_5_6.js
@@ -2,4 +2,8 @@ let montant_ht = prompt("Montant HT ?");
 let taux_tva = prompt("Taux de TVA ?");
 let taux_tva_cal = (taux_tva / 100) + 1;
 let ttc = montant_ht * taux_tva_cal;
-document.body.innerText = ttc; 
+if (ttc > 100){
+    document.body.innerHTML = '<h1 style="color:red;">' + ttc + '</h1>'
+}else{
+    document.body.innerHTML = '<h1 style="color:green;">' + ttc + '</h1>'
+}
